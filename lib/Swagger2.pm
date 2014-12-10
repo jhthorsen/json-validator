231d1790961e9a831c84aaffdbf89b620668346e
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-12-10 12:19:43 +0100

    Released version 0.06
           * Fix t/jv-formats.t: Label not found for "last SKIP"

--- lib/Swagger2.pm
+++ lib/Swagger2.pm
@@ -6,7 +6,7 @@ Swagger2 - Swagger RESTful API Documentation
 
 =head1 VERSION
 
-0.05
+0.06
 
 =head1 DESCRIPTION
 
@@ -54,7 +54,7 @@ use Mojo::Util 'md5_sum';
 use File::Spec;
 use constant CACHE_DIR => $ENV{SWAGGER2_CACHE_DIR} || '';
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 # Should be considered internal
 our $SPEC_FILE = do {
