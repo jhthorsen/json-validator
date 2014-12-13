c11d854a47552628c3f16de0ecde6a5a44887320
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-12-13 12:46:42 +0100

    Released version 0.09
           * Fix load($file) on windows
           * Fix adding parameter to route #1
           * Fix "Resource URL" in generated POD
           * Add "Type" to "Parameters" POD table
           * Disable "int64" format test if Perl does not support 64 bit

--- lib/Swagger2.pm
+++ lib/Swagger2.pm
@@ -6,7 +6,7 @@ Swagger2 - Swagger RESTful API Documentation
 
 =head1 VERSION
 
-0.08
+0.09
 
 =head1 DESCRIPTION
 
@@ -54,7 +54,7 @@ use Mojo::Util 'md5_sum';
 use File::Spec;
 use constant CACHE_DIR => $ENV{SWAGGER2_CACHE_DIR} || '';
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 # Should be considered internal
 our $SPEC_FILE = do {
