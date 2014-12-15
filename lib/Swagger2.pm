2ef87dc1a6496c6128878a253162ab9230afece9
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-12-15 09:29:30 +0100

    Released version 0.11
           * Add "mojo swagger2 pod" command
           * Add "mojo swagger2 perldoc" command
           * Add "mojo swagger2 validate" command
           * Change POD output to follow standard formatting

--- lib/Swagger2.pm
+++ lib/Swagger2.pm
@@ -6,7 +6,7 @@ Swagger2 - Swagger RESTful API Documentation
 
 =head1 VERSION
 
-0.10
+0.11
 
 =head1 DESCRIPTION
 
@@ -54,7 +54,7 @@ use Mojo::Util 'md5_sum';
 use File::Spec;
 use constant CACHE_DIR => $ENV{SWAGGER2_CACHE_DIR} || '';
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 # Should be considered internal
 our $SPEC_FILE = do {
