d16a4c11f4f5b3498b53edfe42f5e5562efa8961
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-01-02 12:44:39 +0100

    Released version 0.16
           * Fix failing Windows test where c:\path\to\spec is converted into
             a URL with scheme "c", #4

--- lib/Swagger2.pm
+++ lib/Swagger2.pm
@@ -6,7 +6,7 @@ Swagger2 - Swagger RESTful API Documentation
 
 =head1 VERSION
 
-0.15
+0.16
 
 =head1 DESCRIPTION
 
@@ -56,7 +56,7 @@ use File::Spec;
 use constant CACHE_DIR => $ENV{SWAGGER2_CACHE_DIR} || '';
 use constant DEBUG     => $ENV{SWAGGER2_DEBUG}     || 0;
 
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 
 # Should be considered internal
 our $SPEC_FILE = File::Spec->catfile(File::Basename::dirname(__FILE__), 'Swagger2', 'schema.json');
