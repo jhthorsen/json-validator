e4510f24d53f1637804ec9a3a86bc13ead5afa48
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-06-24 08:18:44 +0200

    Released version 0.35
     - Add "mojo swagger2 client" command
     - Add EXPERIMENTAL "coerce" attribute to Swagger2::SchemaValidator

--- lib/Swagger2.pm
+++ lib/Swagger2.pm
@@ -6,7 +6,7 @@ Swagger2 - Swagger RESTful API Documentation
 
 =head1 VERSION
 
-0.34
+0.35
 
 =head1 DESCRIPTION
 
@@ -59,7 +59,7 @@ use File::Spec;
 use constant CACHE_DIR => $ENV{SWAGGER2_CACHE_DIR} || '';
 use constant DEBUG     => $ENV{SWAGGER2_DEBUG}     || 0;
 
-our $VERSION = '0.34';
+our $VERSION = '0.35';
 
 # Should be considered internal
 our @SPEC_FILE = (File::Spec->splitdir(File::Basename::dirname(__FILE__)), 'Swagger2', 'schema.json');
