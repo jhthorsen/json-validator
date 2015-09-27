a2fe69c22a35d874c1532a669a459428a97fd257
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-09-27 13:33:06 +0200

    Released version 0.54
     - Add support for $ref to relative path #3 #4 #5
     - Removed Swagger specific type "file"
     - Removed Swagger specific formats: "byte", "date", "double", "float", "int32" and "int64".

--- lib/JSON/Validator.pm
+++ lib/JSON/Validator.pm
@@ -6,7 +6,7 @@ JSON::Validator - Validate data against a JSON schema
 
 =head1 VERSION
 
-0.53
+0.54
 
 =head1 SYNOPSIS
 
@@ -93,7 +93,7 @@ use constant DEBUG => $ENV{JSON_VALIDATOR_DEBUG} || $ENV{SWAGGER2_DEBUG} || 0;
 use constant WARN_ON_MISSING_FORMAT => $ENV{JSON_VALIDATOR_WARN_ON_MISSING_FORMAT}
   || $ENV{SWAGGER2_WARN_ON_MISSING_FORMAT} ? 1 : 0;
 
-our $VERSION   = '0.53';
+our $VERSION   = '0.54';
 our @EXPORT_OK = qw( validate_json );
 
 my $HTTP_SCHEME_RE = qr{^https?:};
