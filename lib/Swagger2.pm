30c0fb65d113992cbf9fe0480d51cd8769c8575d
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-11-19 09:22:47 +0100

    Released version 0.63
     - Add validation of output headers, fixes #41
     - Add support for "readOnly" in object properties #49

--- lib/Swagger2.pm
+++ lib/Swagger2.pm
@@ -6,7 +6,7 @@ Swagger2 - Swagger RESTful API Documentation
 
 =head1 VERSION
 
-0.62
+0.63
 
 =head1 DESCRIPTION
 
@@ -62,7 +62,7 @@ use File::Basename ();
 use File::Spec;
 use Swagger2::SchemaValidator;
 
-our $VERSION = '0.62';
+our $VERSION = '0.63';
 
 # Should be considered internal
 our $SPEC_FILE = File::Spec->catfile(File::Basename::dirname(__FILE__), 'Swagger2', 'schema.json');
