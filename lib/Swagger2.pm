8eb6503ceef2672cec5565527fdeca76f7142ef2
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-11-28 08:18:50 +0100

    Released version 0.64
     - YAML::Tiny is no longer supported by JSON::Validator
     - Bump JSON::Validator dependency to 0.63

--- lib/Swagger2.pm
+++ lib/Swagger2.pm
@@ -6,7 +6,7 @@ Swagger2 - Swagger RESTful API Documentation
 
 =head1 VERSION
 
-0.63
+0.64
 
 =head1 DESCRIPTION
 
@@ -62,7 +62,7 @@ use File::Basename ();
 use File::Spec;
 use Swagger2::SchemaValidator;
 
-our $VERSION = '0.63';
+our $VERSION = '0.64';
 
 # Should be considered internal
 our $SPEC_FILE = File::Spec->catfile(File::Basename::dirname(__FILE__), 'Swagger2', 'schema.json');
