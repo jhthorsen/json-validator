7cfd4a434e94996377be77287bcc3bb34c8f559b
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-04-12 08:32:59 +0200

    Released version 0.77
     - Add Swagger2->find_operations() method
     - Add support for plain WebSocket object passed to ws() in swagger2-client.js
     - Add http() to swagger2-client.js
     - Add load() to swagger2-client.js
     - Change ws() to be a setter and getter in swagger2-client.js
     - Mark "swagger_route_added" as DEPRECATED
     - Remove fresh() and cache features from swagger2-client.js

--- lib/Swagger2.pm
+++ lib/Swagger2.pm
@@ -8,7 +8,7 @@ use File::Basename ();
 use File::Spec;
 use Swagger2::SchemaValidator;
 
-our $VERSION = '0.76';
+our $VERSION = '0.77';
 
 # Should be considered internal
 our $SPEC_FILE = File::Spec->catfile(File::Basename::dirname(__FILE__), 'Swagger2', 'schema.json');
@@ -142,7 +142,7 @@ Swagger2 - Swagger RESTful API Documentation
 
 =head1 VERSION
 
-0.76
+0.77
 
 =head1 DESCRIPTION
 
