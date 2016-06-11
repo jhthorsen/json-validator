2217fbe19de6615961387f9a512a4c17a07ce4c8
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-06-10 19:24:14 -0700

    Released version 0.72
     - Fix default cache_dir() path
     - JSON::Validator is no longer EXPERIMENTAL
     - Move Swagger2::SchemaValidator into JSON::Validator::OpenAPI

--- lib/JSON/Validator.pm
+++ lib/JSON/Validator.pm
@@ -17,7 +17,7 @@ use constant VALIDATE_IP       => eval 'require Data::Validate::IP;1';
 
 use constant DEBUG => $ENV{JSON_VALIDATOR_DEBUG} || 0;
 
-our $VERSION   = '0.71';
+our $VERSION   = '0.72';
 our @EXPORT_OK = qw(validate_json);
 
 my $HTTP_SCHEME_RE = qr{^https?:};
@@ -726,7 +726,7 @@ JSON::Validator - Validate data against a JSON schema
 
 =head1 VERSION
 
-0.71
+0.72
 
 =head1 SYNOPSIS
 
