5cd77a4d190a33522c2d8072fd845a0e6fdd7f77
Anthony Lucas <anthony.lucas@foxtons.co.uk>
2016-06-16 09:41:39 +0100

    only body parameters support a schema, formData does not

--- lib/Swagger2/Client.pm
+++ lib/Swagger2/Client.pm
@@ -137,7 +137,7 @@ sub _validate_request {
           if $type eq 'boolean';
       }
 
-      if ($in eq 'body' or $in eq 'formData') {
+      if ($in eq 'body') {
         warn "[Swagger2::Client] Validate $in\n" if DEBUG;
         push @e,
           map { $_->{path} = $_->{path} eq "/" ? "/$name" : "/$name$_->{path}"; $_; }
