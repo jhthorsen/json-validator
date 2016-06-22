ed766aac2402cd4014f17428ec33a7d85bf4de76
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-06-22 19:27:27 +0200

    Released version 0.82
     - Deprecating Swagger2::SchemaValidator in favor of JSON::Validator::OpenAPI
     - Fix Swagger2::Client formData parameters #87
       Contributor: A.J. Lucas

--- lib/Swagger2.pm
+++ lib/Swagger2.pm
@@ -8,7 +8,7 @@ use File::Basename ();
 use File::Spec;
 use JSON::Validator::OpenAPI;
 
-our $VERSION = '0.81';
+our $VERSION = '0.82';
 
 # Should be considered internal
 our $JS_CLIENT
@@ -138,7 +138,7 @@ Swagger2 - Swagger RESTful API Documentation
 
 =head1 VERSION
 
-0.81
+0.82
 
 =head1 DESCRIPTION
 
