75a89f1479ce5ad65ba4cd372e3461adb8ad804c
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-07-02 14:44:38 +0200

    Released version 0.75
     - Fix uploads must not be slurped
     - Fix reporting error on missing response status definition
     - Add warnings on invalid (Perl) regexes

--- lib/JSON/Validator.pm
+++ lib/JSON/Validator.pm
@@ -17,7 +17,7 @@ use constant VALIDATE_IP       => eval 'require Data::Validate::IP;1';
 
 use constant DEBUG => $ENV{JSON_VALIDATOR_DEBUG} || 0;
 
-our $VERSION   = '0.74';
+our $VERSION   = '0.75';
 our @EXPORT_OK = qw(validate_json);
 
 my $HTTP_SCHEME_RE = qr{^https?:};
@@ -729,7 +729,7 @@ JSON::Validator - Validate data against a JSON schema
 
 =head1 VERSION
 
-0.74
+0.75
 
 =head1 SYNOPSIS
 
