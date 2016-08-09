47aade68d463997a848a78db3f95921cb05f391b
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-08-09 12:45:09 +0200

    Released version 0.82
     - Fix finding all $ref occurances #95

--- lib/JSON/Validator.pm
+++ lib/JSON/Validator.pm
@@ -17,7 +17,7 @@ use constant VALIDATE_IP       => eval 'require Data::Validate::IP;1';
 
 use constant DEBUG => $ENV{JSON_VALIDATOR_DEBUG} || 0;
 
-our $VERSION   = '0.81';
+our $VERSION   = '0.82';
 our @EXPORT_OK = 'validate_json';
 
 my $HTTP_SCHEME_RE = qr{^https?:};
@@ -761,7 +761,7 @@ JSON::Validator - Validate data against a JSON schema
 
 =head1 VERSION
 
-0.81
+0.82
 
 =head1 SYNOPSIS
 
