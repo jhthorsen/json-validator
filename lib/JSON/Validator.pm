d972e5a6cbdb611a61e63d0254377d1a152299bd
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-08-16 19:34:00 +0200

    need to coerce all values... because YAML is awful #27

--- lib/JSON/Validator.pm
+++ lib/JSON/Validator.pm
@@ -157,7 +157,7 @@ sub _load_schema_from_data {
 
 sub _load_schema_from_text {
   return Mojo::JSON::decode_json($_[1]) if $_[1] =~ /^\s*\{/s;
-  $_[0]->{coerce}{booleans} = 1;    # internal coercion
+  $_[0]->coerce(1);    # need to coerce all values... because YAML is awful :(
   local $YAML::Syck::ImplicitTyping = 1;
   _load_yaml($_[1]) || undef;
 }
