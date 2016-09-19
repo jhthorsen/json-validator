a2a13a7df0324b6af29b6af754c0aea09a0fee9a
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-09-19 18:55:13 +0200

    Released version 0.86
     - Deprecated distribution

--- lib/Swagger2.pm
+++ lib/Swagger2.pm
@@ -8,7 +8,7 @@ use File::Basename ();
 use File::Spec;
 use JSON::Validator::OpenAPI;
 
-our $VERSION = '0.85';
+our $VERSION = '0.86';
 
 # Should be considered internal
 our $JS_CLIENT
@@ -138,7 +138,7 @@ Swagger2 - Swagger RESTful API Documentation
 
 =head1 VERSION
 
-0.85
+0.86
 
 =head1 DEPRECATION WARNING
 
