688348efc8b1169b0db2a7231481f7bd25ac52c2
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-10-20 23:00:00 +0200

    Released version 0.87
     - Fix validating data when boolean.pm is loaded

--- lib/JSON/Validator.pm
+++ lib/JSON/Validator.pm
@@ -17,7 +17,7 @@ use constant VALIDATE_IP       => eval 'require Data::Validate::IP;1';
 
 use constant DEBUG => $ENV{JSON_VALIDATOR_DEBUG} || 0;
 
-our $VERSION   = '0.86';
+our $VERSION   = '0.87';
 our @EXPORT_OK = 'validate_json';
 
 my $HTTP_SCHEME_RE = qr{^https?:};
@@ -784,7 +784,7 @@ JSON::Validator - Validate data against a JSON schema
 
 =head1 VERSION
 
-0.86
+0.87
 
 =head1 SYNOPSIS
 
