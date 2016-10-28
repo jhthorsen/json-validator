4ca9c4bd957a09002f97385f2eb1c4bcaa171ddf
Daniel Böhmer <boehmer@strato.de>
2016-10-28 15:05:29 +0200

    OpenAPI::Dancer2: change interface to pass $dsl instead of $app

--- lib/JSON/Validator/OpenAPI/Dancer2.pm
+++ lib/JSON/Validator/OpenAPI/Dancer2.pm
@@ -6,39 +6,39 @@ use Mojo::Base 'JSON::Validator::OpenAPI';
 ### request ###
 
 sub _get_request_data {
-  my ($self, $app, $in) = @_;
+  my ($self, $dsl, $in) = @_;
 
-  return $app->dsl->query_parameters->as_hashref_mixed if $in eq 'query';
-  return $app->dsl->route_parameters->as_hashref_mixed if $in eq 'path';
-  return $app->dsl->body_parameters->as_hashref_mixed  if $in eq 'formData';
-  return Hash::MultiValue->new($app->dsl->request->headers->flatten)->as_hashref_mixed
+  return $dsl->query_parameters->as_hashref_mixed if $in eq 'query';
+  return $dsl->route_parameters->as_hashref_mixed if $in eq 'path';
+  return $dsl->body_parameters->as_hashref_mixed  if $in eq 'formData';
+  return Hash::MultiValue->new($dsl->app->request->headers->flatten)->as_hashref_mixed
     if $in eq 'header';
-  return $app->dsl->request->data if $in eq 'body';
+  return $dsl->app->request->data if $in eq 'body';
   return {};    # TODO correct?
 }
 
 sub _get_request_uploads {
-  my ($self, $app, $name) = @_;
+  my ($self, $dsl, $name) = @_;
 
-  return ($app->request->upload($name));    # context-aware
+  return ($dsl->app->request->upload($name));    # context-aware
 }
 
 sub _set_request_data {
-  my ($self, $app, $in, $name => $value) = @_;
+  my ($self, $dsl, $in, $name => $value) = @_;
 
   if ($in eq 'query') {
-    $app->dsl->query_parameters->set($name => $value);
-    $app->dsl->request->params->{$name} = $value;
+    $dsl->query_parameters->set($name => $value);
+    $dsl->app->request->params->{$name} = $value;
   }
   elsif ($in eq 'path') {
-    $app->dsl->route_parameters->set($name => $value);
+    $dsl->route_parameters->set($name => $value);
   }
   elsif ($in eq 'formData') {
-    $app->dsl->request->body_parameters->set($name => $value);
-    $app->dsl->request->params->{$name} = $value;
+    $dsl->app->request->body_parameters->set($name => $value);
+    $dsl->app->request->params->{$name} = $value;
   }
   elsif ($in eq 'header') {
-    $app->dsl->request->headers->header($name => $value);
+    $dsl->app->request->headers->header($name => $value);
   }
   elsif ($in eq 'body') { }    # no need to write body back
   else {
@@ -50,10 +50,10 @@ sub _set_request_data {
 ### response
 
 sub _get_response_data {
-  my ($self, $app, $in) = @_;
+  my ($self, $dsl, $in) = @_;
 
   if ($in eq 'header') {
-    my @headers = $app->response->headers->flatten;
+    my @headers = $dsl->response->headers->flatten;
     return Hash::MultiValue->new(@headers)->as_hashref_mixed;
   }
 
@@ -61,9 +61,9 @@ sub _get_response_data {
 }
 
 sub _set_response_data {
-  my ($self, $app, $in, $name => $value) = @_;
+  my ($self, $dsl, $in, $name => $value) = @_;
 
-  $in eq 'header' and $app->response->headers->header($name => $value);
+  $in eq 'header' and $dsl->response->headers->header($name => $value);
 
   # TODO what else?
 }
