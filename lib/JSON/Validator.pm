0177a5fdbc5e34c22115842588f8087d6db991cc
Jan Henning Thorsen <jhthorsen@cpan.org>
2017-03-02 22:22:14 +0100

    Released version 0.95
     - Add support for format "password"
     - Add load_and_validate_schema() to JSON::Validator #51
     - Started deprecating load_and_validate_spec()

--- lib/JSON/Validator.pm
+++ lib/JSON/Validator.pm
@@ -18,7 +18,7 @@ use constant SPECIFICATION_URL => 'http://json-schema.org/draft-04/schema#';
 
 use constant DEBUG => $ENV{JSON_VALIDATOR_DEBUG} || 0;
 
-our $VERSION   = '0.94';
+our $VERSION   = '0.95';
 our @EXPORT_OK = 'validate_json';
 
 my $BUNDLED_CACHE_DIR = path(path(__FILE__)->dirname, qw(Validator cache));
@@ -813,7 +813,7 @@ JSON::Validator - Validate data against a JSON schema
 
 =head1 VERSION
 
-0.94
+0.95
 
 =head1 SYNOPSIS
 
