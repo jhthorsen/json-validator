c4ebdf7fa603afd41802265b3b6079b0e5fcddb7
Jan Henning Thorsen <jhthorsen@cpan.org>
2017-06-12 20:58:29 +0200

    Released version 0.99
     - Hopefully fixed some Windows issues #60

--- lib/JSON/Validator.pm
+++ lib/JSON/Validator.pm
@@ -19,7 +19,7 @@ use constant SPECIFICATION_URL => 'http://json-schema.org/draft-04/schema#';
 
 use constant DEBUG => $ENV{JSON_VALIDATOR_DEBUG} || 0;
 
-our $VERSION   = '0.98';
+our $VERSION   = '0.99';
 our @EXPORT_OK = 'validate_json';
 
 my $BUNDLED_CACHE_DIR = path(path(__FILE__)->dirname, qw(Validator cache));
@@ -841,7 +841,7 @@ JSON::Validator - Validate data against a JSON schema
 
 =head1 VERSION
 
-0.98
+0.99
 
 =head1 SYNOPSIS
 
