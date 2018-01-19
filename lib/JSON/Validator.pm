72da48f6241ba811a93358fabeb96394a5380aba
Jan Henning Thorsen <jhthorsen@cpan.org>
2018-01-19 10:36:26 +0100

    Released version 2.00
     - Fix validating against any enum value #22
     - Require YAML::XS 0.67 for proper boolean handling
     - Removed support for parsing YAML with YAML::Syck
     - Removed deprecated method load_and_validate_spec()

--- lib/JSON/Validator.pm
+++ lib/JSON/Validator.pm
@@ -24,7 +24,7 @@ use constant VALIDATE_HOSTNAME => eval 'require Data::Validate::Domain;1';
 use constant VALIDATE_IP       => eval 'require Data::Validate::IP;1';
 
 our $ERR;    # ugly hack to improve validation errors
-our $VERSION   = '1.08';
+our $VERSION   = '2.00';
 our @EXPORT_OK = 'validate_json';
 
 my $BUNDLED_CACHE_DIR = path(path(__FILE__)->dirname, qw(Validator cache));
@@ -990,7 +990,7 @@ JSON::Validator - Validate data against a JSON schema
 
 =head1 VERSION
 
-1.08
+2.00
 
 =head1 SYNOPSIS
 
