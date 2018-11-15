8d3611786b64926d9b3096c7a42867c337a1f58d
Jan Henning Thorsen <jhthorsen@cpan.org>
2018-11-15 16:04:22 +0900

    Released version 2.18
     - Add EXPERIMETNAL support for data:// without a package

--- lib/JSON/Validator.pm
+++ lib/JSON/Validator.pm
@@ -26,7 +26,7 @@ use constant VALIDATE_HOSTNAME => eval 'require Data::Validate::Domain;1';
 use constant VALIDATE_IP       => eval 'require Data::Validate::IP;1';
 
 our $ERR;    # ugly hack to improve validation errors
-our $VERSION   = '2.17';
+our $VERSION   = '2.18';
 our @EXPORT_OK = qw(joi validate_json);
 
 my $BUNDLED_CACHE_DIR = path(path(__FILE__)->dirname, qw(Validator cache));
@@ -1068,7 +1068,7 @@ JSON::Validator - Validate data against a JSON schema
 
 =head1 VERSION
 
-2.17
+2.18
 
 =head1 SYNOPSIS
 
