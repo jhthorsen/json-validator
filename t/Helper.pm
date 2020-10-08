53a3bfab2b96838064348c265649dbba28a8ad17
Jan Henning Thorsen <jhthorsen@cpan.org>
2020-10-09 07:27:06 +0900

    Using Test::JSON::Schema::Acceptance for draft4 acceptance tests #213

--- t/Helper.pm
+++ t/Helper.pm
@@ -2,11 +2,45 @@ package t::Helper;
 use Mojo::Base -base;
 
 use JSON::Validator;
-use Mojo::JSON 'encode_json';
-use Mojo::Util 'monkey_patch';
+use Mojo::JSON qw(decode_json encode_json);
+use Mojo::Util qw(monkey_patch);
 use Test::More;
 
 $ENV{TEST_VALIDATOR_CLASS} = 'JSON::Validator';
+
+sub acceptance {
+  my ($class, $schema_class, %acceptance_params) = @_;
+
+  Test::More::plan(skip_all => 'cpanm Test::JSON::Schema::Acceptance')
+    unless eval 'require Test::JSON::Schema::Acceptance;1';
+  Test::More::plan(skip_all => $@) unless eval "require $schema_class;1";
+
+  $acceptance_params{todo_tests}
+    = [map { +{file => $_->[0], group_description => $_->[1], test_description => $_->[2]}; }
+      @{$acceptance_params{todo_tests}}]
+    if $acceptance_params{todo_tests};
+
+  my $specification = $schema_class =~ m!::(\w+)$! ? lc $1 : 'unknown';
+  Test::JSON::Schema::Acceptance->new(specification => $specification)->acceptance(
+    %acceptance_params,
+    validate_data => sub {
+      my ($schema_p, $data_p) = map { Mojo::JSON::Pointer->new(shift @_) } qw(schema data);
+      my ($schema,   $data)   = map { clone($_->data) } $schema_p, $data_p;
+      my $valid = $schema_class->new($schema)->validate($data) ? 0 : 1;
+
+      # Doing internal tests on mutation, since I think Test::JSON::Schema::Acceptance is a bit too strict
+      Test2::Tools::Compare::is(encode_json($data),   encode_json($data_p->data),   'data structure is the same');
+      Test2::Tools::Compare::is(encode_json($schema), encode_json($schema_p->data), 'schema structure is the same')
+        unless _acceptance_schema_contains_invalid_ref($schema_p);
+
+      return $valid;
+    },
+  );
+}
+
+sub clone {
+  return decode_json(encode_json($_[0]));
+}
 
 sub edj {
   return Mojo::JSON::decode_json(Mojo::JSON::encode_json(@_));
@@ -14,13 +48,10 @@ sub edj {
 
 sub joi_ok {
   my ($data, $joi, @expected) = @_;
-  my $description
-    ||= @expected ? "errors: @expected" : "valid: " . encode_json($data);
+  my $description ||= @expected ? "errors: @expected" : "valid: " . encode_json($data);
   my @errors = JSON::Validator::Joi->new($joi)->validate($data);
-  Test::More::is_deeply(
-    [map { $_->TO_JSON } sort { $a->path cmp $b->path } @errors],
-    [map { $_->TO_JSON } sort { $a->path cmp $b->path } @expected],
-    $description)
+  Test::More::is_deeply([map { $_->TO_JSON } sort { $a->path cmp $b->path } @errors],
+    [map { $_->TO_JSON } sort { $a->path cmp $b->path } @expected], $description)
     or Test::More::diag(encode_json(\@errors));
 }
 
@@ -30,15 +61,12 @@ sub schema { state $schema; $schema = $_[1] if $_[1]; $schema }
 
 sub schema_validate_ok {
   my ($data, $schema, @expected) = @_;
-  my $description
-    = @expected ? "errors: @expected" : "valid: " . encode_json($data);
+  my $description = @expected ? "errors: @expected" : "valid: " . encode_json($data);
 
   my @errors = t::Helper->schema->resolve($schema)->validate($data);
   local $Test::Builder::Level = $Test::Builder::Level + 1;
-  Test::More::is_deeply(
-    [map { $_->TO_JSON } sort { $a->path cmp $b->path } @errors],
-    [map { $_->TO_JSON } sort { $a->path cmp $b->path } @expected],
-    $description)
+  Test::More::is_deeply([map { $_->TO_JSON } sort { $a->path cmp $b->path } @errors],
+    [map { $_->TO_JSON } sort { $a->path cmp $b->path } @expected], $description)
     or Test::More::diag(encode_json(\@errors));
 }
 
@@ -51,14 +79,11 @@ sub test {
 
 sub validate_ok {
   my ($data, $schema, @expected) = @_;
-  my $description
-    = @expected ? "errors: @expected" : "valid: " . encode_json($data);
-  my @errors = jv()->schema($schema)->validate($data);
+  my $description = @expected ? "errors: @expected" : "valid: " . encode_json($data);
+  my @errors      = jv()->schema($schema)->validate($data);
   local $Test::Builder::Level = $Test::Builder::Level + 1;
-  Test::More::is_deeply(
-    [map { $_->TO_JSON } sort { $a->path cmp $b->path } @errors],
-    [map { $_->TO_JSON } sort { $a->path cmp $b->path } @expected],
-    $description)
+  Test::More::is_deeply([map { $_->TO_JSON } sort { $a->path cmp $b->path } @errors],
+    [map { $_->TO_JSON } sort { $a->path cmp $b->path } @expected], $description)
     or Test::More::diag(encode_json(\@errors));
 }
 
@@ -79,6 +104,19 @@ sub import {
   monkey_patch $caller => schema_validate_ok => \&schema_validate_ok;
   monkey_patch $caller => true               => \&Mojo::JSON::true;
   monkey_patch $caller => validate_ok        => \&validate_ok;
+}
+
+sub _acceptance_schema_contains_invalid_ref {
+  my $p     = shift;
+  my @paths = ('', '/properties/foo');
+
+  # JSON::Validator always normalizes $ref with multiple keys
+  for my $path (@paths) {
+    my $ref = $p->get($path);
+    return 1 if ref $ref eq 'HASH' && $ref->{'$ref'} && 1 != keys %$ref;
+  }
+
+  return 0;
 }
 
 1;
