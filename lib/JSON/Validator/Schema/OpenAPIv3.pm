efeabe989f4b131b58326d7dfd74e2e50a68b6ad
Jan Henning Thorsen <jhthorsen@cpan.org>
2020-10-31 11:04:50 +0900

    Add OpenAPIv3::parameters_for_request() and parameters_for_response()

--- lib/JSON/Validator/Schema/OpenAPIv3.pm
+++ lib/JSON/Validator/Schema/OpenAPIv3.pm
@@ -1,8 +1,55 @@
 package JSON::Validator::Schema::OpenAPIv3;
 use Mojo::Base 'JSON::Validator::Schema::OpenAPIv2';
 
+use JSON::Validator::Util qw(schema_type);
+
 has moniker       => 'openapiv3';
 has specification => 'https://spec.openapis.org/oas/3.0/schema/2019-04-02';
+
+sub parameters_for_request {
+  my $self = shift;
+  my ($method, $path) = (lc $_[0][0], $_[0][1]);
+
+  my $cache_key = "parameters_for_request:$method:$path";
+  return $self->{cache}{$cache_key} if $self->{cache}{$cache_key};
+  return undef unless $self->get([paths => $path, $method]);
+
+  my @parameters = map {@$_} $self->_find_all_nodes([paths => $path, $method], 'parameters');
+  for my $param (@parameters) {
+    $param->{type} ||= schema_type($param->{schema});
+  }
+
+  if (my $request_body = $self->get([paths => $path, $method, 'requestBody'])) {
+    my @accepts = sort keys %{$request_body->{content} || {}};
+    push @parameters, {accepts => \@accepts, in => 'body', name => 'body', required => $request_body->{required}};
+  }
+
+  return $self->{cache}{$cache_key} = \@parameters;
+}
+
+sub parameters_for_response {
+  my $self = shift;
+  my ($method, $path, $status) = (lc $_[0][0], $_[0][1], $_[0][2] || 200);
+
+  $status ||= 200;
+  my $cache_key = "parameters_for_response:$method:$path:$status";
+  return $self->{cache}{$cache_key} if $self->{cache}{$cache_key};
+
+  my $responses = $self->get([paths => $path, $method, 'responses']);
+  my $response  = $responses->{$status} || $responses->{default};
+  return undef unless $response;
+
+  my @parameters;
+  if (my $headers = $response->{headers}) {
+    push @parameters, map { +{%{$headers->{$_}}, in => 'header', name => $_} } sort keys %$headers;
+  }
+
+  if (my @accepts = sort keys %{$response->{content} || {}}) {
+    push @parameters, {accepts => \@accepts, in => 'body', name => 'body'};
+  }
+
+  return $self->{cache}{$cache_key} = \@parameters;
+}
 
 sub _build_formats {
 
@@ -70,6 +117,40 @@ Used to get/set the moniker for the given schema. Default value is "openapiv2".
 Defaults to "L<http://swagger.io/v2/schema.json>".
 
 =head1 METHODS
+
+=head2 parameters_for_request
+
+  $parameters = $schema->parameters_for_request([$method, $path]);
+
+Finds all the request parameters defined in the schema, including inherited
+parameters. Returns C<undef> if the C<$path> and C<$method> cannot be found.
+
+Example return value:
+
+  [
+    {in => "query", name => "q"},
+    {in => "body", name => "body", accepts => ["application/json"]},
+  ]
+
+The return value MUST not be mutated.
+
+=head2 parameters_for_response
+
+  $array_ref = $schema->parameters_for_response([$method, $path, $status]);
+
+Finds the response parameters defined in the schema. Returns C<undef> if the
+C<$path>, C<$method> and C<$status> cannot be found. Will default to the
+"default" response definition if C<$status> could not be found and "default"
+exists.
+
+Example return value:
+
+  [
+    {in => "header", name => "X-Foo"},
+    {in => "body", name => "body", accepts => ["application/json"]},
+  ]
+
+The return value MUST not be mutated.
 
 =head1 SEE ALSO
 
