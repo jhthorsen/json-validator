6617df87764331c3f810868730f9da3fc1e85ba4
Stephan Hradek <stephan.hradek@eco.de>
2021-01-26 13:13:41 +0100

    Make date-time regexp RFC3339 compatible

--- lib/JSON/Validator/Formats.pm
+++ lib/JSON/Validator/Formats.pm
@@ -31,8 +31,10 @@ sub check_date {
 }
 
 sub check_date_time {
-  my @dt = $_[0] =~ m!^(\d{4})-(\d\d)-(\d\d)[T ](\d\d):(\d\d):(\d\d(?:\.\d+)?)(?:Z|([+-])(\d+):(\d+))?$!io;
+  my @dt = $_[0] =~ m!^(\d{4})-(\d\d)-(\d\d)[T ](\d\d):(\d\d):(\d\d(?:\.\d+)?)(?:Z|([+-])(\d\d):(\d\d))?$!io;
   return 'Does not match date-time format.' unless @dt;
+  return 'Time offset hour out of range.'    if defined $dt[7] and $dt[7] > 23;
+  return 'Time offset minute out of range.'  if defined $dt[8] and $dt[8] > 59;
   @dt = map { s/^0//; $_ } reverse @dt[0 .. 5];
   $dt[4] -= 1;    # month are zero based
   local $@;
